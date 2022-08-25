2f97192150aadf6d1bad7b7b21178a630be824e1
pzh <pengzhonghua@greatsoft.net>
2022-08-25 18:43:40 +0800

    config git hook

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugins from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugins({
+      // 配置项
+      cache: false
+    })
+  ],
   server: {
     host: '0.0.0.0'
   }
